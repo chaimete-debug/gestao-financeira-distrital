736c9905e7214bd6a2f9a5b62c29f3fd4edaabbc
chaimete-debug <chaimete@gmail.com>
2026-05-13 01:20:52 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-/* Configuração do frontend.
-   Deve ficar na mesma pasta do index.html no Vercel/GitHub. */
-var APP_CONFIG = window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwf_AzYbpwDpqC0U1iV3m-3_xP4oKHlMIj3JTiWn7N9lHeabp8cAf6nEXiK-xNqTFJH/exec"
-};
