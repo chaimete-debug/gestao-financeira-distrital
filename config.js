bdf1e9a4642dbd72ac0e4c2bc5ff335f60a8a965
chaimete-debug <chaimete@gmail.com>
2026-05-13 13:52:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
    Deve ficar na mesma pasta do index.html no Vercel/GitHub. */
 var APP_CONFIG = window.APP_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbzcfkTKIFZ8Zx1lqUlH4VxfQEhhI5upAj4uX3DxD0pixEvRAJYC5yEqF7SFoCNJc5gv/exec"
-};
+}; 
  
