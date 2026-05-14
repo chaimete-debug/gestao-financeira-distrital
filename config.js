68cf1d53cab6ef9e46294856a9b43a9e4122e8c3
chaimete-debug <chaimete@gmail.com>
2026-05-14 21:54:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 var APP_CONFIG = window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbzcfkTKIFZ8Zx1lqUlH4VxfQEhhI5upAj4uX3DxD0pixEvRAJYC5yEqF7SFoCNJc5gv/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbwf_AzYbpwDpqC0U1iV3m-3_xP4oKHlMIj3JTiWn7N9lHeabp8cAf6nEXiK-xNqTFJH/exec"
 };
